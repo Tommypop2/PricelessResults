a8ab6dd2c64d06a43d80d218bed75a4bb81699ff
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-05-02 14:10:54 +0100

    Added background gradient to dark mode

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 			</div>
 			<div class="absolute right-1 bottom-1 <sm:flex <sm:flex-row <sm:items-center <sm:justify-center <sm:w-full <sm:bottom-16 <sm:right-0">
 				<div
-					class="absolute w-full h-full z-[-1] animate-pulse animate-duration-[5s] hover:animate-paused"
+					class="absolute w-full h-full z-[0] animate-pulse animate-duration-[5s] hover:animate-paused"
 					style={{
 						"background-image":
 							"linear-gradient(-120deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
@@ -39,7 +39,7 @@ export default function Home() {
 				/>
 				<img
 					src="512x512.webp"
-					class="relative hover:animate-pulse w-[400px] h-[400px] <sm:w-[100%] <sm:h-[auto] object-cover"
+					class="relative hover:animate-pulse w-[400px] h-[400px] <sm:w-[100%] <sm:h-[auto] object-cover z-[1]"
 					draggable={false}
 					alt="Logo"
 				></img>
