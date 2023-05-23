d5f8681114256361cd6799af66e2f35fdde132aa
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-05-23 19:41:46 +0100

    Added chartjs, which can actually run on the server

--- frontend/src/routes/user/dashboard/(dashboard).tsx
+++ frontend/src/routes/user/dashboard/(dashboard).tsx
@@ -1,5 +1,28 @@
-import { Show, Suspense, createSignal, lazy, onMount } from "solid-js";
-
+import { onMount } from "solid-js";
+import { Chart, Title, Tooltip, Legend, Colors } from "chart.js";
+import { Line } from "solid-chartjs";
 export default function Dashboard() {
-	return <>Dashboard Page</>;
+	onMount(() => {
+		Chart.register(Title, Tooltip, Legend, Colors);
+	});
+
+	const chartData = {
+		labels: ["January", "February", "March", "April", "May"],
+		datasets: [
+			{
+				label: "Sales",
+				data: [50, 60, 70, 80, 90],
+			},
+		],
+	};
+	const chartOptions = {
+		responsive: true,
+		maintainAspectRatio: false,
+	};
+
+	return (
+		<div>
+			<Line data={chartData} options={chartOptions} width={500} height={500} />
+		</div>
+	);
 }
