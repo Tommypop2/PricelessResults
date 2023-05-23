2a56da6f195e2ae6c010c35e180a874044392aa3
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-05-23 17:40:15 +0100

    Removed apex charts

--- frontend/src/routes/user/dashboard/(dashboard).tsx
+++ frontend/src/routes/user/dashboard/(dashboard).tsx
@@ -1,22 +1,5 @@
 import { Show, Suspense, createSignal, lazy, onMount } from "solid-js";
 
-const ChartComponent = lazy(() => import("../../../charts"));
-
-function ClientOnly(props: any) {
-	const [flag, setFlag] = createSignal(false);
-
-	onMount(() => {
-		setFlag(true);
-	});
-
-	return <Show when={flag()}>{props.children}</Show>;
-}
 export default function Dashboard() {
-	return (
-		<ClientOnly>
-			<Suspense>
-				<ChartComponent />
-			</Suspense>
-		</ClientOnly>
-	);
+	return <>Dashboard Page</>;
 }
